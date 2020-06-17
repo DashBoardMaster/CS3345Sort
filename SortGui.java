8ed2f8250d0ef688e829fdb13edbc56f4cd28c2f
4D617278 <4D617278@protonmail.com>
2020-06-16 22:25:49 -0500

    Progress on insertion sort

--- SortGui.java
+++ SortGui.java
@@ -1,13 +1,30 @@
+import java.util.Arrays;
 public class SortGui 
 {
+	int[] data;
 	public SortGui() 
 	{
-		
+		data = new int[50];
+	}
+	public SortGui(int[] data) 
+	{
+		this.data = data;
 	}
 	
 	void insertionSort() 
 	{
-
+		for(int i = 1; i < data.length; i++)
+		{
+			int elementToSort = data[i];
+			int indexToInsert = i;
+			for(int j = 0; j < i; j++)
+			{
+				if(data[j] > data[i]) 
+				{
+					indexToInsert = j;
+				}
+			}
+		}
 	}
 	
 	void selectionSort() 
