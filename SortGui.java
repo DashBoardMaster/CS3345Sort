ceb241f999c30c8849218b8039d713e4beb4e20f
4D617278 <4D617278@protonmail.com>
2020-06-17 00:34:34 -0500

    Added insertion sort and random number generator

--- SortGui.java
+++ SortGui.java
@@ -1,27 +1,64 @@
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SortGui 
 {
-	int[] data;
+	public int[] data;
+	
 	public SortGui() 
 	{
 		data = new int[50];
 	}
+	
 	public SortGui(int[] data) 
 	{
 		this.data = data;
+	}
+
+	void printData()
+	{
+		for(int i = 0; i < data.length; i++)
+		{
+			if (i == data.length - 1)
+			{
+				System.out.print(data[i]);
+			}
+			else
+			{
+				System.out.print(data[i] + ",");
+			}			
+		}
+		System.out.println();
+	}
+	
+	/* Generates 50000 random numbers from 1 to 1000000 
+	 * Duplicates allowed
+	 * Store in array 
+	 */
+	static int[] randomNumberGenerator() 
+	{
+		int[] rand = new int[10];
+		for(int i = 0; i < 10; i++)
+		{
+			rand[i] = ThreadLocalRandom.current().nextInt(1, 1000001);
+		}
+		return rand;
 	}
 	
 	void insertionSort() 
 	{
 		for(int i = 1; i < data.length; i++)
 		{
-			int elementToSort = data[i];
-			int indexToInsert = i;
-			for(int j = 0; j < i; j++)
+			for(int j = i - 1; j >= 0; j--)
 			{
 				if(data[j] > data[i]) 
 				{
-					indexToInsert = j;
+					// Swap values
+					data[i] = data[i] + data[j];
+					data[j] = data[i] - data[j];
+					data[i] = data[i] - data[j];
+					// Decrement i
+					i--; 
 				}
 			}
 		}
@@ -54,6 +91,11 @@ public class SortGui
 	
 	public static void main(String[] args) 
 	{
-    
+		SortGui sortGui = new SortGui(randomNumberGenerator());
+		System.out.println("Before sorting: ");
+		sortGui.printData();
+		sortGui.insertionSort();
+		System.out.println("After sorting: ");
+		sortGui.printData();
 	}
 }
