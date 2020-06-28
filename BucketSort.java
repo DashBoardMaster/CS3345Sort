1c28c8f18f95965e34911329933953caed54f1db
4D617278 <4D617278@protonmail.com>
2020-06-28 11:24:29 -0500

    Fixed BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -1,24 +1,22 @@
 public class BucketSort {
   /** Bucket sort method */
-  public static <E extends Iterable<E>>void bucketSort(E[] list) {
-    E[] bucket = (E[])new java.util.ArrayList[t+1];
+  public void bucketSort(int[] list) {
+  	
+    int[] bucket = new int[list.length + 1];
 
     // Distribute the elements from list to buckets
     for (int i = 0; i < list.length; i++) {
-      int key = list[i].getKey(); // Assume element has the getKey() method
-
-      if (bucket[key] == null)
-        bucket[key] = new java.util.ArrayList<>();
-
-      bucket[key].add(list[i1);
+      int key = list[i]; 
+      bucket[key] += 1;
     }
 
     // Now move the elements from the buckets back to list
     int k = 0; // k is an index for list
     for (int i = 0; i < bucket.length; i++) {
-      if (bucket[i] != null) {
-        for (int j = 0; j < bucket[i].size(); j++) 
-          list[k++] = bucket[i].get(j);
+      if (bucket[i] != 0) {
+        for (int j = 0; j < bucket.length; j++) 
+          list[k++] = bucket[i];
       }
     }
+  }
 }
