92d6986099d7022814a7147bebd322443473573d
4D617278 <4D617278@protonmail.com>
2020-06-28 23:31:43 -0500

    Update on Gui (10 minute increments)

--- QuickSort.java
+++ QuickSort.java
@@ -66,62 +66,62 @@ public class QuickSort {
 			return first;
 		}
 	}
-	public static void main(String[] args) {
-		QuickSort.comparisons = 0;
-		QuickSort.movements = 0;
-		for(int i = 10; i < 10001; i *= 10)
-		{
-			int[] list = SortGui.sortedDataGenerator(i);
-			long start = System.nanoTime();
-			QuickSort.quickSort(list);
-			long end = System.nanoTime();
-			long time = end - start;
-			System.out.print(QuickSort.comparisons + " ");
-			System.out.print(QuickSort.movements + " ");
-			System.out.println(time);
-		}
-		QuickSort.comparisons = 0;
-		QuickSort.movements = 0;
-		for(int i = 10; i < 10001; i *= 10)
-		{
-			int[] list = SortGui.reverseDataGenerator(i);
-			long start = System.nanoTime();
-			QuickSort.quickSort(list);
-			long end = System.nanoTime();
-			long time = end - start;
-			System.out.print(QuickSort.comparisons + " ");
-			System.out.print(QuickSort.movements + " ");
-			System.out.println(time);
-		}
-		QuickSort.comparisons = 0;
-		QuickSort.movements = 0;
-		for(int i = 10; i < 10001; i *= 10)
-		{
-			int[] list1 = SortGui.sortedDataGenerator((int) (i * 0.8));
-			int[] list2 = SortGui.randomNumberGenerator((int) (i * 0.2));
-			int[] list = new int[list1.length + list2.length];
-			System.arraycopy(list1, 0, list, 0, list1.length);
-			System.arraycopy(list2, 0, list, list1.length, list2.length);
-			long start = System.nanoTime();
-			QuickSort.quickSort(list);
-			long end = System.nanoTime();
-			long time = end - start;
-			System.out.print(QuickSort.comparisons + " ");
-			System.out.print(QuickSort.movements + " ");
-			System.out.println(time);
-		}
-		QuickSort.comparisons = 0;
-		QuickSort.movements = 0;
-		for(int i = 10; i < 10001; i *= 10)
-		{
-			int[] list = SortGui.randomNumberGenerator(i);
-			long start = System.nanoTime();
-			QuickSort.quickSort(list);
-			long end = System.nanoTime();
-			long time = end - start;
-			System.out.print(QuickSort.comparisons + " ");
-			System.out.print(QuickSort.movements + " ");
-			System.out.println(time);
-		}
-	}
+//	public static void main(String[] args) {
+//		QuickSort.comparisons = 0;
+//		QuickSort.movements = 0;
+//		for(int i = 10; i < 10001; i *= 10)
+//		{
+//			int[] list = SortGui.sortedDataGenerator(i);
+//			long start = System.nanoTime();
+//			QuickSort.quickSort(list);
+//			long end = System.nanoTime();
+//			long time = end - start;
+//			System.out.print(QuickSort.comparisons + " ");
+//			System.out.print(QuickSort.movements + " ");
+//			System.out.println(time);
+//		}
+//		QuickSort.comparisons = 0;
+//		QuickSort.movements = 0;
+//		for(int i = 10; i < 10001; i *= 10)
+//		{
+//			int[] list = SortGui.reverseDataGenerator(i);
+//			long start = System.nanoTime();
+//			QuickSort.quickSort(list);
+//			long end = System.nanoTime();
+//			long time = end - start;
+//			System.out.print(QuickSort.comparisons + " ");
+//			System.out.print(QuickSort.movements + " ");
+//			System.out.println(time);
+//		}
+//		QuickSort.comparisons = 0;
+//		QuickSort.movements = 0;
+//		for(int i = 10; i < 10001; i *= 10)
+//		{
+//			int[] list1 = SortGui.sortedDataGenerator((int) (i * 0.8));
+//			int[] list2 = SortGui.randomNumberGenerator((int) (i * 0.2));
+//			int[] list = new int[list1.length + list2.length];
+//			System.arraycopy(list1, 0, list, 0, list1.length);
+//			System.arraycopy(list2, 0, list, list1.length, list2.length);
+//			long start = System.nanoTime();
+//			QuickSort.quickSort(list);
+//			long end = System.nanoTime();
+//			long time = end - start;
+//			System.out.print(QuickSort.comparisons + " ");
+//			System.out.print(QuickSort.movements + " ");
+//			System.out.println(time);
+//		}
+//		QuickSort.comparisons = 0;
+//		QuickSort.movements = 0;
+//		for(int i = 10; i < 10001; i *= 10)
+//		{
+//			int[] list = SortGui.randomNumberGenerator(i);
+//			long start = System.nanoTime();
+//			QuickSort.quickSort(list);
+//			long end = System.nanoTime();
+//			long time = end - start;
+//			System.out.print(QuickSort.comparisons + " ");
+//			System.out.print(QuickSort.movements + " ");
+//			System.out.println(time);
+//		}
+//	}
 }
