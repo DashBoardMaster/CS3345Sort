ab7a832c08954a936ada8d376e25a38edc0020af
4D617278 <4D617278@protonmail.com>
2020-06-28 20:29:36 -0500

    Update on Gui (10 minute increments)

--- SelectionSort.java
+++ SelectionSort.java
@@ -9,9 +9,9 @@ public class SelectionSort {
 			int currentMin = list[i];
 			int currentMinIndex = i;
 
-			for (int j =i + 1; j < list.length; j++) {
+			for (int j = i + 1; j < list.length; j++) {
+				comparisons++;
 				if (currentMin > list[j]) {
-					comparisons++;
 					currentMin = list[j];
 					currentMinIndex = j;
 				}
@@ -20,7 +20,7 @@ public class SelectionSort {
 			// Swap list[i] with list[currentMinIndex] if necessary
 			if (currentMinIndex != i) {
 				comparisons++;
-				list[currentMinIndex] = list[i];jhtts://os.google.om/oument//1EBHbVtLA7gvKNI4AEwg6__BTevzLhNy9T6_R6jg/eitjhtts://o	js.goohgle.tts:o/m/oosu.mgeont/o/g1lEeB.HboVm/toAugmveKnNtI/4A/E1wEgB6H_b_VBTtejLhAv7tzgtLvs:/K/os.goNhojgINle.oy4hm/ou9TAment//1ETBHbEV6jwctSLA_7gvG:kNh6R/i4^_taE/wjg6__G_Bo?BTevejzESLpdhNhy.iv/9TJGTtjz/th6_ottRhs^Litojtcs.goopjosgl	tge.om/hou:hhtmetss/ETNt.	t/:/./y1Eo9B:oH?BC:/Tm/Vgds/6jct?LO.?DECHAog7vODRt.skNCI4Ao6tJEuscjogmhwg6>SGMO_gls_eg/jt.oBeojTnOEHG:LhhLTP?OGCNti/gy9etso6t_.e6jgs/.je.s1di:/ote:hm.on/soTBt.	/t/C?ODHg/pdjmodsmdobdose./cVjhoc:scgd1unjtcuhg.uom/tEtsltmgh/OP>LetjotnudDGSAjhttj.nbtm/ohot:7slhs:e/gtonEb/djsld/ot/g.tvmBC?V.heDLSP?:K?HgS?OEntc.:/jle.cooMI/o;oBu//hHc.osmHA/sojtde/ublA/1tgmE:/b7
+				list[currentMinIndex] = list[i];
 				list[i] = currentMin;
 				movements++;
 			}
@@ -28,9 +28,42 @@ public class SelectionSort {
 	}
 	
 	public static void main(String[] args) {
+		SelectionSort.comparisons = 0;
+		SelectionSort.movements = 0;
+		System.out.println("Random");
 		for(int i = 10; i < 10001; i *= 10)
 		{
 			int[] list = SortGui.randomNumberGenerator(i);
+			long start = System.nanoTime();
+			SelectionSort.selectionSort(list);
+			long end = System.nanoTime();
+			long time = end - start;
+			System.out.println("Size: " + i);
+			System.out.print(SelectionSort.comparisons + " ");
+			System.out.print(SelectionSort.movements + " ");
+			System.out.println(time);
+		}
+		SelectionSort.comparisons = 0;
+		SelectionSort.movements = 0;
+		System.out.println("Sorted");
+		for(int i = 10; i < 10001; i *= 10)
+		{
+			int[] list = SortGui.sortedDataGenerator(i);
+			long start = System.nanoTime();
+			SelectionSort.selectionSort(list);
+			long end = System.nanoTime();
+			long time = end - start;
+			System.out.println("Size: " + i);
+			System.out.print(SelectionSort.comparisons + " ");
+			System.out.print(SelectionSort.movements + " ");
+			System.out.println(time);
+		}
+		SelectionSort.comparisons = 0;
+		SelectionSort.movements = 0;
+		System.out.println("Reverse");
+		for(int i = 10; i < 10001; i *= 10)
+		{
+			int[] list = SortGui.reverseDataGenerator(i);
 			long start = System.nanoTime();
 			SelectionSort.selectionSort(list);
 			long end = System.nanoTime();
